a2ff16e2971e6f0761fe92819b0975bd723334ed
Matheus Bezerra Lima <matheusbeducation@gmail.com>
2024-05-06 15:25:35 -0300

    Arrumar-carrinho
    Z-index do carrinho está errado

--- site AFM/script.js
+++ site AFM/script.js
@@ -1,9 +1,25 @@
+const topHeader = document.querySelector('.top-header')
+const header = document.querySelector('.hdr')
+const nav = document.querySelector('.nav-list')
+
+
 const mostrarCarrinho = () =>{
     const abaCar = document.querySelector('.aba-carrinho-aberta')
     abaCar.style.transform = ' translateX(0%)' 
+    document.body.style.overflowY = 'hidden'
 }
 
 const fecharCarrinho = () =>{
     const abaCar = document.querySelector('.aba-carrinho-aberta')
     abaCar.style.transform = ' translateX(100%)' 
+    document.body.style.overflowY = 'scroll'
 }
+
+const activeScroll = () =>{
+    topHeader.classList.toggle('sumir' , window.scrollY > 0)
+    header.classList.toggle('sumir' ,window.scrollY > 0)
+    nav.classList.toggle('nav-scroll' , window.scrollY > 0 )
+    nav.style.position = 'fixed'
+}
+
+window.addEventListener('scroll', activeScroll)
